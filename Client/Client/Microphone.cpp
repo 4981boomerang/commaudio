ddc7797cb23f67749d1f34d06bed717fc3b30661
CerberusOne <aingaran2003@gmail.com>
2017-04-05 12:33:57 -0700

    adding microphone feature

---
+++ Client/Client/Microphone.cpp
@@ -0,0 +1,48 @@
+#include "Microphone.h"
+
+using namespace libZPlay;
+
+DWORD WINAPI microphoneStart(LPVOID lpParam) {
+	ZPlay* player;
+
+	player = CreateZPlay();
+
+	if (player == 0) {
+		perror("Microphone: Player failed!");
+		return 0;
+	}
+
+	int ver = player->GetVersion();	//get library version
+
+	//check version
+	if (ver < 200) {
+		perror("Microphone: version 2.00 or above required!");
+		return 0;
+	}
+
+	// open wavein using Line In
+	if (player->OpenFile("wavein://src=line;volume=50;", sfAutodetect) == 0) {
+		perror("Microphone: Line in file failed!");
+		player->Release();
+		return 0;
+	}
+
+	// set wave output to disk file, mp3 encoding
+	if (player->SetWaveOutFile(OUTPUT_FILE, sfMp3, 1) == 0)	{
+		perror("Microphone: ouput file failed!");
+		player->Release();
+		return 0;
+	}
+
+	// start playing
+	if (player->Play() == 0) {
+		perror("Microphone: Play failed!");
+		player->Release(); // delete ZPlay class
+		return 0;
+	}
+
+	player->SetMasterVolume(0, 0);	//mute the user's voice echo
+
+
+	return 0;
+}
